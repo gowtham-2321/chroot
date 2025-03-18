3b39e2ed4b08510d4ae324e9f42672e09be6a909
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-03-18 21:14:28 +0530

    next app setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl text-indigo-500 font-bold">
+        hello
+      </p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
